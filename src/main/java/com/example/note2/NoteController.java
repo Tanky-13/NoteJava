de1c87a8078d34c0233e2f8ea74d5358d61828a8
Tanky-13 <uyket@inbox.ru>
2024-03-06 15:34:10 +0600

    teg03

--- src/main/java/com/example/note2/NoteController.java
+++ src/main/java/com/example/note2/NoteController.java
@@ -3,7 +3,7 @@ package com.example.note2;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/example/note2/Notebook.java
+++ src/main/java/com/example/note2/Notebook.java
@@ -11,7 +11,6 @@ public class Notebook {
     private String name;
     private String content;
 
-    // Getters and Setters
     public Long getId() {
         return id;
     }
